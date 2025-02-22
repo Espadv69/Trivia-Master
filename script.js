39e8345260ad76e764092622bea1f17f23e8b500
Espadv69 <aitorjob69@gmail.com>
2025-02-22 13:07:05 +0100

    Add DOM elements into variables

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ import questions from './questions'
 
 let currentQuestionIndex = 0
 let score = 0
+
+// DOM elements
+const $questionElement = document.querySelector('.question')
+const $answersElement = document.querySelector('.answers')
+const $scoreElement = document.querySelector('.score')
+const $nextButton = document.querySelector('.next-btn')
