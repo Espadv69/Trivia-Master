eaee6dcf35ab122f020b40a28144d802b974ed85
Espadv69 <aitorjob69@gmail.com>
2025-02-22 12:59:17 +0100

    Add question about "Don Quixote" by Miguel de Cervantes

--- questions.js
+++ questions.js
@@ -4,4 +4,14 @@ const questions = [
     options: ['Madrid', 'Rome', 'London', 'Paris', 'Berlin'],
     answer: 'Paris',
   },
+  {
+    question: 'Who wrote "Don Quixote"?',
+    options: [
+      'Miguel de Cervantes',
+      'Gabriel García Márquez',
+      'Pablo Neruda',
+      'Federico García Lorca',
+    ],
+    answer: 'Miguel de Cervantes',
+  },
 ]
