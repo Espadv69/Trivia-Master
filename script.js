c44fcd6050b32cfc741e0c2c2a38dbf83e81f66c
Espadv69 <aitorjob69@gmail.com>
2025-02-22 13:09:23 +0100

    Add start button functionality to initiate the quiz

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const $questionElement = document.querySelector('.question')
 const $answersElement = document.querySelector('.answers')
 const $scoreElement = document.querySelector('.score')
 const $nextButton = document.querySelector('.next-btn')
+
+document.querySelector('.start-btn').addEventListener('click', startQuiz)
+
+function startQuiz() {
+  document.querySelector('start-btn').style.display = 'none'
+  document.querySelector('.quiz-container').style.display = 'block'
+}
