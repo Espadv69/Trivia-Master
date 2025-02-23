378daab3f2e3fa8872da3879757757f6147a2ac1
Espadv69 <aitorjob69@gmail.com>
2025-02-23 20:39:48 +0100

    Add categories to questions for better organization and context

--- questions.js
+++ questions.js
@@ -1,10 +1,12 @@
 const questions = [
   {
+    category: 'Geography', // Categoría
     question: 'What is the capital of France?',
     options: ['Rome', 'London', 'Paris', 'Berlin'],
     answer: 'Paris',
   },
   {
+    category: 'History',
     question: 'Who wrote "Don Quixote"?',
     options: [
       'Miguel de Cervantes',
@@ -15,26 +17,31 @@ const questions = [
     answer: 'Miguel de Cervantes',
   },
   {
+    category: 'Science',
     question: 'What is the largest ocean in the world?',
     options: ['Atlantic', 'Pacific', 'Indian', 'Arctic'],
     answer: 'Pacific',
   },
   {
+    category: 'History',
     question: 'In what year did man land on the moon?',
     options: ['1969', '1959', '1979', '1989'],
     answer: '1969',
   },
   {
+    category: 'Science',
     question: 'What is the chemical symbol for gold?',
     options: ['Ag', 'Au', 'Pb', 'Fe'],
     answer: 'Au',
   },
   {
+    category: 'Science',
     question: 'Which planet is known as the red planet?',
     options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
     answer: 'Mars',
   },
   {
+    category: 'Art',
     question: 'Who painted the Mona Lisa?',
     options: [
       'Vicent van Gogh',
@@ -45,26 +52,31 @@ const questions = [
     answer: 'Leonardo da Vinci',
   },
   {
+    category: 'Science',
     question: 'What is the powerhouse of the cell?',
     options: ['Nucleus', 'Ribosome', 'Mitochondria', 'Chloroplast'],
     answer: 'Mitochondria',
   },
   {
+    category: 'Geography',
     question: 'Which country is famous for sushi?',
     options: ['China', 'Korea', 'Japan', 'Vietnam'],
     answer: 'Japan',
   },
   {
+    category: 'Geography',
     question: 'What is the longest river in the world?',
     options: ['Amazon', 'Nile', 'Yangtze', 'Mississippi'],
     answer: 'Nile',
   },
   {
+    category: 'Geography',
     question: 'How many continents are there on Earth?',
     options: ['5', '6', '7', '8'],
     answer: '7',
   },
   {
+    category: 'Science',
     question: 'Who developed the theory of relativity?',
     options: [
       'Isaac Newton',
@@ -75,16 +87,19 @@ const questions = [
     answer: 'Albert Einstein',
   },
   {
+    category: 'Food',
     question: 'What is the main ingredient in guacamole?',
     options: ['Tomato', 'Onion', 'Avocado', 'Pepper'],
     answer: 'Avocado',
   },
   {
+    category: 'Science',
     question: 'Which gas do plants absorb from the atmosphere?',
     options: ['Oxygen', 'Nitrogen', 'Carbon Dioxide', 'Hydrogen'],
     answer: 'Carbon Dioxide',
   },
   {
+    category: 'Geography',
     question: 'What is the capital of Spain?',
     options: ['Madrid', 'Barcelona', 'Valencia', 'Seville'],
     answer: 'Madrid',
